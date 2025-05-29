0edc5d02cb612bd6ae2b6c8bbd195a71febdd31a
Prabhani <p.vilochanie2000@gmail.com>
2025-05-29 09:48:28 +0530

    Home page added and place chatbot in chat icon

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -3,23 +3,25 @@ import '../Styles/Navbar.css';
 import logo from '../assets/logo.png';
 import { FiSearch, FiMessageCircle } from 'react-icons/fi';
 import { FaUserCircle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       {/* Left: Logo */}
       <div className="navbar-left">
-        <img src={logo} alt="PathPilot Logo" className="navbar-logo" />
-        
+        <Link to="/">
+          <img src={logo} alt="PathPilot Logo" className="navbar-logo" />
+        </Link>
       </div>
 
       {/* Center: Navigation Links */}
       <div className="navbar-links">
-        <a href="#">Home</a>
-        <a href="#">Services</a>
-        <a href="#">Dashboard</a>
-        <a href="#">Contact</a>
-        <a href="#">About us</a>
+        <Link to="/">Home</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/about">About us</Link>
       </div>
 
       {/* Right: Search bar, chat icon, user icon */}
@@ -28,7 +30,8 @@ const Navbar = () => {
           <FiSearch />
           <input type="text" placeholder="Search" />
         </div>
-        <FiMessageCircle className="navbar-icon" />
+         <Link to="/chatbot">
+    <FiMessageCircle className="navbar-icon" /></Link>
         <FaUserCircle className="navbar-icon" />
       </div>
     </nav>
