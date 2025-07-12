4760574ffa2eccb31bf8db48e0d4614044f0e10a
WaruniSandareka <warunisandareka963@gmail.com>
2025-07-12 19:21:50 +0530

    Add urls for get user details

--- backend/path_pilot_app/urls.py
+++ backend/path_pilot_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
     path("chatbot_response/", views.chatbot_response, name="chatbot_response"),
+    path('user/', views.user_details, name='user_details'),
 ]
