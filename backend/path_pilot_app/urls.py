cd779e9328b99c7c4fe4036a8bcd8ed16869f9b2
WaruniSandareka <warunisandareka963@gmail.com>
2025-07-15 23:41:40 +0530

    Modify urlto get response

--- backend/path_pilot_app/urls.py
+++ backend/path_pilot_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("chatbot_response/", views.chatbot_response, name="chatbot_response"),
     path('user/', views.user_details, name='user_details'),
     path('resume/', views.generate_resume, name='generate_resume'),
+    path('career-assessment/', views.career_assessment_create, name='create_career_assessment'),
 ]
